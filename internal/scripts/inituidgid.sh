2d029441ddd3bdee0b4c9a7c330f66b7f8f5de8c
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-11-18 11:34:33 +0100

    Disallow running scripts as any user that conflicts with the user database inside the container

--- internal/scripts/inituidgid.sh
+++ internal/scripts/inituidgid.sh
@@ -2,6 +2,20 @@
 # Changes the 'app' user's UID and GID to the values specified
 # in $APP_UID and $APP_GID.
 set -e
+set -o pipefail
+
+if [[ "$APP_UID" -lt 1024 ]]; then
+	if awk -F: '{ print $3 }' < /etc/passwd | grep -q "^${APP_UID}$"; then
+		echo "ERROR: you can only run this script with a user whose UID is at least 1024, or whose UID does not already exist in the Docker container. Current UID: $APP_UID"
+		exit 1
+	fi
+fi
+if [[ "$APP_GID" -lt 1024 ]]; then
+	if awk -F: '{ print $4 }' < /etc/passwd | grep -q "^${APP_GID}$"; then
+		echo "ERROR: you can only run this script with a user whose GID is at least 1024, or whose GID does not already exist in the Docker container. Current GID: $APP_GID"
+		exit 1
+	fi
+fi
 
 chown -R "$APP_UID:$APP_GID" /home/app
 groupmod -g "$APP_GID" app
