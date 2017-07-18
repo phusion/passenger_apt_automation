6488ffaae44e1e143b1be4dfeedd05f70921b093
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2017-07-18 19:40:08 +0200

    Apply previous commit fixes to Enterprise as well.
    Also remove unused method.

--- internal/build/rakefile_support.rb
+++ internal/build/rakefile_support.rb
@@ -97,10 +97,3 @@ def infer_next_passenger_version(passenger_version)
   components.join(".")
 end
 
-def nginx_version_for_distro(distro)
-  if dynamic_module_supported?(distro)
-    latest_nginx_available(distro)
-  else
-    PhusionPassenger::PACKAGING_PREFERRED_NGINX_VERSION
-  end
-end
