6a615dc3af971ca4eab989b191a1f7fece86e7c8
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2018-05-07 12:33:18 +0200

    Fixup overzealous removal of helper code for Wheezy drop.

--- debian_specs/passenger/helpers.rb
+++ debian_specs/passenger/helpers.rb
@@ -1,15 +1,21 @@
 def ruby_package_dependencies
   case distribution_class
   when :ubuntu
-    if is_distribution?("<= artful")
+    if is_distribution?("<= trusty")
+      "ruby1.9.1, ruby1.9.1-dev, ruby2.0, ruby2.0-dev"
+    elsif is_distribution?("<= artful")
       "ruby2.3, ruby2.3-dev"
     else
       # At least up to, and including, v18.04 Bionic 
       "ruby2.5, ruby2.5-dev"
     end
   when :debian
-    # At least up to, and including, v9 Stretch
-    "ruby2.3, ruby2.3-dev"
+    if is_distribution?("<= jessie")
+      "ruby2.1, ruby2.1-dev"
+    else
+      # At least up to, and including, v9 Stretch
+      "ruby2.3, ruby2.3-dev"
+    end
   else
     raise "Unknown distribution class"
   end
@@ -20,15 +26,21 @@ end
 def distro_ruby_versions
   case distribution_class
   when :ubuntu
-    if is_distribution?("<= artful")
+    if is_distribution?("<= trusty")
+      ["1.9.1", "2.0"]
+    elsif is_distribution?("<= artful")
       ["2.3"]
     else
       # At least up to, and including, v18.04 Bionic 
       ["2.5"]
     end
   when :debian
-    # At least up to, and including, v9 Stretch
-    ["2.3"]
+    if is_distribution?("<= jessie")
+      ["2.1"]
+    else
+      # At least up to, and including, v9 Stretch
+      ["2.3"]
+    end
   else
     raise "Unknown distribution class"
   end
