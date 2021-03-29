59e61b81f476a0cef5ee036f356f23b7a9307f37
Camden Narzt <c.narzt@me.com>
2021-03-29 11:03:46 -0600

    bump build and testboxes versions

--- internal/lib/docker_image_info.sh
+++ internal/lib/docker_image_info.sh
@@ -18,11 +18,11 @@ fi
 if [[ "$1" == "buildbox_name" ]]; then
 	echo -n "phusion/passenger_apt_automation_buildbox"
 elif [[ "$1" == "buildbox_version" ]]; then
-	echo -n "1.1.8"
+	echo -n "1.1.9"
 elif [[ "$1" == "testbox_base_name" ]]; then
 	echo -n "phusion/passenger_apt_automation_testbox"
 elif [[ "$1" == "testbox_version" ]]; then
-	echo -n "1.0.7"
+	echo -n "1.0.8"
 else
 	echo "$USAGE"
 	exit 1
