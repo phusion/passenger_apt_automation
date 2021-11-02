babe031ac13b5497bc4683312d66a3b1eabca45e
Camden Narzt <c.narzt@me.com>
2021-11-01 18:08:23 -0600

    ensure distro-info-data is updated when building buildbox

--- docker-images/buildbox/install.sh
+++ docker-images/buildbox/install.sh
@@ -53,7 +53,7 @@ run chown app: ~app/.pbuilderrc
 header "Installing dependencies"
 run apt-get install -y -q ubuntu-dev-tools debhelper source-highlight \
 	ruby ruby-dev libsqlite3-dev runit git gawk dh-make dirmngr debian-keyring \
-	zlib1g-dev libxml2-dev libxslt1-dev gdebi-core gnupg dh-systemd
+	zlib1g-dev libxml2-dev libxslt1-dev gdebi-core gnupg dh-systemd distro-info-data
 run gem install rake bundler --no-document
 run gem update --system 3.1.5 --no-document
 run env BUNDLE_GEMFILE=/paa_build/Gemfile bundle install
