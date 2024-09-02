0c037198c7d23a11395ac28301b1968816c047fd
Camden Narzt <c.narzt@me.com>
2024-09-02 13:20:24 -0600

    shellcheck recommendations

--- internal/build/setup-environment.sh
+++ internal/build/setup-environment.sh
@@ -6,29 +6,29 @@
 # times from the shell, through initpbuilder.
 
 set -e
-ROOTDIR=`dirname "$0"`
-ROOTDIR=`cd "$ROOTDIR/../.." && pwd`
+ROOTDIR="$(dirname "$0")"
+ROOTDIR="$(cd "$ROOTDIR/../.." && pwd)"
 source "$ROOTDIR/internal/lib/library.sh"
 source "$ROOTDIR/internal/lib/distro_info.sh"
 
 require_args_exact 2 "$@"
 DISTRO="$1"
 ARCH="$2"
-HOSTARCH=`dpkg --print-architecture`
+HOSTARCH="$(dpkg --print-architecture)"
 
-if [[ $ARCH == $HOSTARCH ]]; then
+if [[ $ARCH == "$HOSTARCH" ]]; then
 	BASE_TGZ="$DISTRO-base.tgz"
 else
 	BASE_TGZ="$DISTRO-$ARCH-base.tgz"
 fi
 
-known_distro $DISTRO
+known_distro "$DISTRO"
 
 if [[ ! -e /cache/base-$DISTRO-$ARCH.tgz ]]; then
 	echo "+ yes | pbuilder-dist $DISTRO $ARCH create --updates-only"
-	yes | pbuilder-dist $DISTRO $ARCH create --updates-only
-	run mv ~/pbuilder/$BASE_TGZ /cache/base-$DISTRO-$ARCH.tgz
+	yes | pbuilder-dist "$DISTRO" "$ARCH" create --updates-only
+	run mv "$HOME/pbuilder/$BASE_TGZ" "/cache/base-$DISTRO-$ARCH.tgz"
 fi
-if [[ ! -e ~/pbuilder/$BASE_TGZ ]]; then
-	run ln -s /cache/base-$DISTRO-$ARCH.tgz ~/pbuilder/$BASE_TGZ
+if [[ ! -e "$HOME/pbuilder/$BASE_TGZ" ]]; then
+	run ln -s "/cache/base-$DISTRO-$ARCH.tgz" "$HOME/pbuilder/$BASE_TGZ"
 fi
