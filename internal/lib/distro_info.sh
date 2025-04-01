8ee86e29cb920b17bea856f5921196ef9e0453e5
Camden Narzt <c.narzt@me.com>
2025-04-01 12:43:44 -0600

    bump buildbox to have new distro info

--- internal/lib/distro_info.sh
+++ internal/lib/distro_info.sh
@@ -46,7 +46,7 @@ function to_distro_codename()
 
 function get_buildbox_image()
 {
-	echo "phusion/passenger_apt_automation_buildbox:2.2.1"
+	echo "phusion/passenger_apt_automation_buildbox:2.2.2"
 }
 
 function to_testbox_image()
