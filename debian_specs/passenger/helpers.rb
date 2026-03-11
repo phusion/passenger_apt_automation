d6de1ebe2d02be6bc3b40e30bfa5b134f7afa5d9
Camden Narzt <c.narzt@me.com>
2026-03-10 22:51:42 -0600

    remove no-longer-supported distros from ruby list and simplify maintenance

--- debian_specs/passenger/helpers.rb
+++ debian_specs/passenger/helpers.rb
@@ -1,45 +1,5 @@
 def ruby_package_dependencies
-  case distribution_class
-  when :ubuntu
-    if is_distribution?("<= artful")
-      "ruby2.3, ruby2.3-dev"
-    elsif is_distribution?("<= eoan")
-      # v18.04 Bionic -> v19.10 Eoan
-      "ruby2.5, ruby2.5-dev"
-    elsif is_distribution?("<= impish")
-      # v20.04 Focal -> v21.10 Impish
-      "ruby2.7, ruby2.7-dev"
-    elsif is_distribution?("<= kinetic")
-      # v22.04 Jolly -> v22.10 Kinetic
-      "ruby3.0, ruby3.0-dev"
-    elsif is_distribution?("<= mantic")
-      # v23.04 Lunar -> v23.10 Mantic
-      "ruby3.1, ruby3.1-dev"
-    elsif is_distribution?("== noble")
-      # v24.04 Noble
-      "ruby3.2, ruby3.2-dev"
-    else
-      # v24.10 Oracular -> ???
-      "ruby3.3, ruby3.3-dev"
-    end
-  when :debian
-    if is_distribution?("<= jessie")
-      "ruby2.1, ruby2.1-dev"
-    elsif is_distribution?("<= stretch")
-      "ruby2.3, ruby2.3-dev"
-    elsif is_distribution?("<= buster")
-      "ruby2.5, ruby2.5-dev"
-    elsif is_distribution?("<= bullseye")
-      "ruby2.7, ruby2.7-dev"
-    elsif is_distribution?("<= bookworm")
-      "ruby3.1, ruby3.1-dev"
-    else
-      # trixie
-      "ruby3.3, ruby3.3-dev"
-    end
-  else
-    raise "Unknown distribution class"
-  end
+  distro_ruby_versions.map { |ver| "ruby#{ver}, ruby#{ver}-dev" }.join(", ")
 end
 
 # Returns the Ruby versions available for a given distribution.
@@ -47,33 +7,23 @@ end
 def distro_ruby_versions
   case distribution_class
   when :ubuntu
-    if is_distribution?("<= artful")
-      ["2.3"]
-    elsif is_distribution?("<= eoan")
-      ["2.5"]
-    elsif is_distribution?("<= impish")
+    if is_distribution?("<= impish") # 21.10
       ["2.7"]
-    elsif is_distribution?("<= kinetic")
+    elsif is_distribution?("<= kinetic") # 22.10
       ["3.0"]
-    elsif is_distribution?("<= mantic")
+    elsif is_distribution?("<= mantic") # 23.10
       ["3.1"]
-    elsif is_distribution?("== noble")
+    elsif is_distribution?("== noble") # 24.04
       ["3.2"]
-    else
+    else # 24.10+
       ["3.3"]
     end
   when :debian
-    if is_distribution?("<= jessie")
-      ["2.1"]
-    elsif is_distribution?("<= stretch")
-      ["2.3"]
-    elsif is_distribution?("<= buster")
-      ["2.5"]
-    elsif is_distribution?("<= bullseye")
+    if is_distribution?("<= bullseye")
       ["2.7"]
     elsif is_distribution?("<= bookworm")
       ["3.1"]
-    else
+    else # trixie+
       ["3.3"]
     end
   else
