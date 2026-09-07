22a744423e5aba41ad54ee5f39b4fcd37ac20e76
Camden Narzt <c.narzt@me.com>
2026-09-07 14:00:54 -0600

    debug stonking in CI
    [ci skip] [ci:skip]

--- internal/scripts/inituidgid.sh
+++ internal/scripts/inituidgid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Changes the 'app' user's UID and GID to the values specified
 # in $APP_UID and $APP_GID.
-set -e
+set -ex
 set -o pipefail
 
 # Hack to make the Passenger Apt packaging tests on our Jenkins infrastructure work. Jenkins has UID 999/1000 and GID 998.
@@ -29,13 +29,6 @@ fi
 chown -R "$APP_UID:$APP_GID" /home/app
 groupmod -g "$APP_GID" app
 usermod -u "$APP_UID" -g "$APP_GID" app
-
-# There's something strange with either Docker or the kernel, so that
-# the 'app' user cannot access its home directory even after a proper
-# chown/chmod. We work around it like this.
-mv /home/app /home/app2
-cp -dpR /home/app2 /home/app
-rm -rf /home/app2
 
 if [[ $# -gt 0 ]]; then
 	exec "$@"
